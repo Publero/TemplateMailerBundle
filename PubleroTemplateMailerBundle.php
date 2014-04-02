cc3d5d7c35324843dd1f78f70217d98ade0430f1
Tomáš Pecsérke <tomas.pecserke@publero.com>
2014-04-02 16:29:54 +0200

    used universal mappings & PSR-4 autoloader

--- PubleroTemplateMailerBundle.php
+++ PubleroTemplateMailerBundle.php
@@ -13,31 +13,8 @@ namespace Publero\TemplateMailerBundle;
 
 use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
 use Doctrine\Bundle\MongoDBBundle\DependencyInjection\Compiler\DoctrineMongoDBMappingsPass;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Pecserke\Component\UniversalMappings\HttpKernel\Bundle\UniversalMappingsBundle;
 
-class PubleroTemplateMailerBundle extends Bundle
+class PubleroTemplateMailerBundle extends UniversalMappingsBundle
 {
-    public function build(ContainerBuilder $container)
-    {
-        $mappings = array(
-            realpath(__DIR__ . '/Resources/config/doctrine/model') => 'Publero\TemplateMailerBundle\Model',
-        );
-
-        if (class_exists('Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass')) {
-            $container->addCompilerPass(DoctrineOrmMappingsPass::createYamlMappingDriver(
-                $mappings,
-                array('publero_token_authentication.model_manager_name'),
-                'publero_template_mailer.backend.orm'
-            ));
-        }
-
-        if (class_exists('Doctrine\Bundle\MongoDBBundle\DependencyInjection\Compiler\DoctrineMongoDBMappingsPass')) {
-            $container->addCompilerPass(DoctrineMongoDBMappingsPass::createYamlMappingDriver(
-                $mappings,
-                array('publero_token_authentication.model_manager_name'),
-                'publero_template_mailer.backend.mongodb'
-            ));
-        }
-    }
 }
